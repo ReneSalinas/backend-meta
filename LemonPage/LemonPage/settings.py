6de98f73063906c1374344bdeda1306f14dfd920
ReneSalinas <renesalinas0525@gmail.com>
2024-11-04 17:30:50 -0600

    template

--- LemonPage/LemonPage/settings.py
+++ LemonPage/LemonPage/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'LemonPage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'mainapp/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
