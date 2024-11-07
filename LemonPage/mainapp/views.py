cb3ac8249eceb2f368110b872a31594bfeb9baf4
ReneSalinas <renesalinas0525@gmail.com>
2024-11-07 17:47:07 -0600

    templates heredadas

--- LemonPage/mainapp/views.py
+++ LemonPage/mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from datetime import datetime
 from mainapp.forms import BookingForm
+from .models import *
 
 def index(request): 
     return HttpResponse("Hello, world. This is the index view of mainapp.") 
@@ -22,19 +23,6 @@ def drinks(request, drink_name):
     choice_of_drink = drinks[drink_name]
     return HttpResponse(f"<h2> {drink_name} </h2>" + choice_of_drink)
 
-def home(request):
-    return HttpResponse("Welcome to Little Lemon!")
-
-def about(request):
-    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12- 15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
-    return render(request, 'about.html', {'content': about_content})
-
-def menu(request):
-    return HttpResponse("Menu")
-
-def book(request):
-    return HttpResponse("Make a booking")
-
 def form_view(request):
     form = BookingForm()
     if request.method == "POST":
@@ -43,3 +31,15 @@ def form_view(request):
             form.save()
     context = {"form":form}
     return render(request, "booking.html", context)
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def book(request):
+    return render(request, 'book.html')
